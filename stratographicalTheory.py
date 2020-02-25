4cb0fa9ecb968a4f8f37deeaf547dfdfb6a2857a
Luke Haskins <deut31v6@comcast.net>
2020-02-24 23:59:15 -0500

    Created stratographicalTheory.py

---
+++ stratographicalTheory.py
@@ -0,0 +1,7 @@
+#stratographicalTheory.py
+#Luke Haskins
+#trivia_prep
+
+#Intro
+print This program is to test your memorization of the various stages of earth's formation according to modern American theory.
+
