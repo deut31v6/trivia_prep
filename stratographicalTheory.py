a851110ee255404002baa277738a507d3a161976
Luke Haskins <deut31v6@comcast.net>
2020-02-25 00:28:14 -0500

    Made non-working version of random number generator

--- stratographicalTheory.py
+++ stratographicalTheory.py
@@ -5,4 +5,6 @@
 #Intro
 print "This program is to test your memorization of the various stages of earth's formation according to modern American theory."
 
-#
+#Basic version: Create a random number (A year between 0 and 4.6 billion) and have a user name the eon to which it belongs
+randNum = random.randint(1,4600000000)
+print randNum
